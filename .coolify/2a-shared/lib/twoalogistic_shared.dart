bb302d322501904d7c637abbc0a1af9d36e14216
Ostaapich <43037186+Ostaapich@users.noreply.github.com>
2026-04-26 18:16:35 +0300

    Vendor shared package for Coolify web build

---
+++ .coolify/2a-shared/lib/twoalogistic_shared.dart
@@ -0,0 +1,15 @@
+library twoalogistic_shared;
+
+// Chat models
+export 'src/models/chat/chat_message.dart';
+export 'src/models/chat/chat_conversation.dart';
+export 'src/models/chat/chat_attachment.dart';
+
+// Track models
+export 'src/models/track/product_info.dart';
+
+// Common models
+export 'src/models/common/delta_event.dart';
+
+// Services
+export 'src/services/websocket_service.dart';
